f582b01df2723fe94c59b113373cfa586e9831a4
aldairdiaz <irving.s21diaz@gmail.com>
2023-03-08 17:31:31 -0600

    "Define models"

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,25 @@
+import datetime
 from django.db import models
 
-# Create your models here.
+
+class Ingredients(models.Model):
+    name = models.CharField(max_length=30)
+    quantity = models.FloatField(default=0)
+    unit = models.CharField(max_length=10, default='kg')
+    price = models.FloatField(default=0)
+
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=30)
+    price = models.FloatField(default=0)
+
+
+class RecipeRequirements(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredients = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
+    quantity = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
+
+
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(datetime.date.today)
