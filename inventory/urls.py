4f3c1ad8749261c9b108430851734ef4571efdfe
aldairdiaz <irving.s21diaz@gmail.com>
2023-03-11 08:53:47 -0600

    "Define Views and paths"

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.Home.as_view(), name='home'),
+    path('ingredients/', views.ViewIngredients.as_view(), name='ingredients'),
+    path('ingredients/delete/<pk>/', views.DeleteIngredients.as_view(), name="delete-ingredients"),
+    path('menu/', views.ViewMenuItems.as_view(), name='menu-items'),
+    path('purchase/', views.ViewPurchase.as_view(), name='purchase'),
+    path('profit/', views.ViewProfit.as_view(), name='profit')
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.views.generic import ListView, TemplateView
+from django.views.generic.edit import DeleteView  # CreateView, UpdateView,
+from .models import Ingredients, MenuItem, RecipeRequirements, Purchase
 
 
+class Home(TemplateView):
+    template_name = ""
+
+
+class ViewIngredients(ListView):
+    model = Ingredients
+    template_name = ""
+
+
+class DeleteIngredients(DeleteView):
+    model = Ingredients
+    template_name = ""
+
+
+class ViewMenuItems(ListView):
+    model = MenuItem
+    template_name = ""
+
+
+class ViewPurchase(ListView):
+    model = Purchase
+    template_name = ""
+
+
+class ViewProfit(ListView):
+    model = RecipeRequirements
+    template_name = ""
